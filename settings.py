751ded25ab21757a847b10fb7f39cb36cee31943
joseph <hong.joseph97@gmail.com>
2023-09-02 12:16:59 -0400

    added settings

---
+++ settings.py
@@ -0,0 +1,13 @@
+"""
+File: settings.py
+Just some globl variables used across scripts for now.
+"""
+
+def init():
+    global dive_count
+    global raid_count
+    global game_pos
+
+    dive_count = 0
+    raid_count = 0
+    game_pos = [-1, -1] # position of game icon in upper left corner
